7913908b99b39b76074b24d02fb8a6e507ce6b1e
Supriya Debnath <0xsupriya@gmail.com>
2025-07-29 10:22:46 +0530

    "refactored the files"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { SrcModule } from './lib/common/src/src.module';
 import { UserModule } from './user/user.module';
 import { CollabModule } from './collab/collab.module';
 import { ExploreModule } from './explore/explore.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, ExploreModule, CollabModule, UserModule, SrcModule],
+  imports: [AuthModule, ExploreModule, CollabModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
