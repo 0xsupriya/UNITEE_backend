4e2553433b7f5d05702a2f6511f19031f88fda1d
Supriya Debnath <0xsupriya@gmail.com>
2025-07-29 09:56:47 +0530

    "setup folder structure for MVP features"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CollabModule } from './collab/collab.module';
+import { ExploreModule } from './explore/explore.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ExploreModule, CollabModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
