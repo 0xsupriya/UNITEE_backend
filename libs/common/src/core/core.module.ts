24863af2d8b677741c64e983644e6b9f3ddd780c
Supriya Debnath <0xsupriya@gmail.com>
2025-07-30 06:37:59 +0530

    Configuration: global configuration module, Typeorm module and Throttleer module for rate limitting

---
+++ libs/common/src/core/core.module.ts
@@ -0,0 +1,38 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from "@nestjs/common";
+import { databaseConfig } from "../config";
+import {ThrottlerModule} from '@nestjs/throttler';
+@Module({
+    imports: [
+        // Global configuration module
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [databaseConfig]
+        }),
+
+        // Typeorm module configuration
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => {
+                const dbConfig = configService.get('database');
+                if(!dbConfig) {
+                    throw new Error('Database configuration is missing');
+                }
+                return dbConfig;
+            },
+            inject: [ConfigService],
+        }),
+        
+        // Throttler module for rate limitting
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 60,
+                    limit: 10,
+                },
+            ],
+        }),
+    ],
+})
+export class CoreModule{}
