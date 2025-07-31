c5f158cb02109b798c22305553b0e7137e89ee84
Supriya Debnath <0xsupriya@gmail.com>
2025-07-31 06:18:02 +0530

    "added userEntity to database config"

--- libs/common/src/config/database.config.ts
+++ libs/common/src/config/database.config.ts
@@ -1,9 +1,13 @@
 import { registerAs } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { userEntity } from "libs/db/entities/user.entity";
 export const databaseConfig = registerAs(
     'database', () => ({
         type: 'postgres',
         url: process.env.DATABASE_URL,
+        entities: [
+            userEntity
+        ],
         synchronize: true,
     }) as TypeOrmModuleOptions,
 )
