6cd1daf2edb1df4dd4f63fad26ca1d0672f45e4c
Supriya Debnath <0xsupriya@gmail.com>
2025-08-03 22:55:38 +0530

    "Auth Module"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { userEntity } from '../../libs/db/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([userEntity]),
+    JwtModule.register({}), // config is loaded from env
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
