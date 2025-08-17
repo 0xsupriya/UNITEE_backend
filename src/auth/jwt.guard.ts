018e79d0447e00742c5b9eb24402f5a7d7e33638
Supriya Debnath <0xsupriya@gmail.com>
2025-08-17 12:09:13 +0530

    Refactored

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {} // Tells NestJS to use the "jwt" strategy
+export class JwtGuard extends AuthGuard('jwt') {} // Tells NestJS to use the "jwt" strategy 
