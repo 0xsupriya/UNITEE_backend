7830e67335f443bb2ea2b838d317c4fa101c7058
Supriya Debnath <0xsupriya@gmail.com>
2025-08-17 13:11:27 +0530

    Auth Controller: getMe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,26 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body,Get, Req, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto } from "./dto/signUp.dto";
 import { SignInDto } from "./dto/signIn.dto";
-
+import { JwtGuard } from "./jwt.guard";
 @Controller('auth')
 export class AuthController{
     constructor(
         private authService: AuthService ){}
     @Post('sign-up')
    signUp(@Body() signUpDto: SignUpDto){
-    return this.authService.signUp(signUpDto)
+    return this.authService.signUp(signUpDto);
    }
 
    @Post('sign-in')
    signIn(@Body() signInDto: SignInDto){
-    return this.authService.signIn(signInDto)
+    return this.authService.signIn(signInDto);
    }
+
+   @UseGuards(JwtGuard)
+   @Get('me')
+   getMe(@Req() req: Request){
+    return (req as any).user;
+   }
+
 }
