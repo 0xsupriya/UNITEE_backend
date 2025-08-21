04f9eccbf73e72fe62c8538aa7af7bbb662264bb
Supriya Debnath <0xsupriya@gmail.com>
2025-08-22 05:25:43 +0530

    created dto for get user profiles

---
+++ src/user-profile/dto/get-user-profiles.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsString, IsArray } from "class-validator";
+
+export class GetUserProfilesDto {
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsOptional()
+  @IsString()
+  experience?: string;
+
+  @IsOptional()
+  @IsArray()
+  techstack?: string[];
+
+  @IsOptional()
+  @IsString()
+  sort?: "recent" | "most-active" | "most-connections";
+}
