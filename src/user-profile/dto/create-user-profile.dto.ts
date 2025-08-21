8a7d17995046ce96daaca008550e0682ed86aecc
Supriya Debnath <0xsupriya@gmail.com>
2025-08-21 21:24:30 +0530

    created DTOs for users profile

--- src/user-profile/dto/create-user-profile.dto.ts
+++ src/user-profile/dto/create-user-profile.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserProfileDto {}
+import { IsString, IsOptional, IsArray, IsUrl } from 'class-validator';
+
+export class CreateUserProfileDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsUrl()
+  github?: string;
+
+  @IsOptional()
+  @IsUrl()
+  linkedin?: string;
+
+  @IsOptional()
+  @IsArray()
+  techstack?: string[];
+
+  @IsOptional()
+  @IsString()
+  profilePic?: string;
+
+  @IsOptional()
+  @IsString()
+  experience?: string;
+}
