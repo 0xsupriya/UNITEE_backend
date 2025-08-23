e02627685d32686f24d62390e54345fe00cd61ca
Supriya Debnath <0xsupriya@gmail.com>
2025-08-23 16:47:24 +0530

    created collab module

--- src/collab/collab.module.ts
+++ src/collab/collab.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollabService } from './collab.service';
 import { CollabController } from './collab.controller';
+import { CollabPost } from 'libs/db/entities/collab-post.entity';
+import { CollabApplication } from 'libs/db/entities/collab-application.entity';
+import { UserProfile } from 'libs/db/entities/user-profile.entity';
 
 @Module({
-  controllers: [CollabController],
+  imports: [
+    TypeOrmModule.forFeature([CollabPost, CollabApplication, UserProfile]),
+  ],
   providers: [CollabService],
+  controllers: [CollabController],
 })
 export class CollabModule {}
