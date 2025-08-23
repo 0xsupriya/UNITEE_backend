09327e8cb8b0593fcfa0d0aa21f73f3a21972dcc
Supriya Debnath <0xsupriya@gmail.com>
2025-08-23 10:13:17 +0530

    created dto for filter users

---
+++ src/explore/dto/filter-users.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsString, IsArray, IsIn } from 'class-validator';
+
+export class FilterUsersDto {
+  @IsOptional()
+  @IsString()
+  role?: string;   // e.g., "Frontend Developer"
+
+  @IsOptional()
+  @IsString()
+  experience?: string; // e.g., "Beginner" | "Intermediate" | "Expert"
+
+  @IsOptional()
+  @IsArray()
+  techstack?: string[];  // e.g., ["react", "nodejs"]
+
+  @IsOptional()
+  @IsIn(["most_active", "recently_joined", "most_connections"])
+  sortBy?: string;
+}
