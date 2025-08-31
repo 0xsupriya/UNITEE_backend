362c9ba79592547a0e698bf32172fd7d18fc78ab
Supriya Debnath <0xsupriya@gmail.com>
2025-08-31 18:14:00 +0530

    fixed schema of env

--- libs/common/src/config/env.schema.ts
+++ libs/common/src/config/env.schema.ts
@@ -6,7 +6,7 @@ export const envSchema = z.object({
     .string()
     .min(32, 'JWT_SECRET must be at least 32 characters long'),
   NODE_ENV: z
-    .enum(['development', 'production', 'test'])
+    .enum(['development', 'production'])
     .default('development'),
   FRONTEND_URL: z.string().optional(),
 });
